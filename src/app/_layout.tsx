18934e0c961c0bda6078272381d86cf20ff0a4e6
sdsasdfd <unasmirza316@gmail.com>
2026-01-30 11:12:57 +0500

    change nav bg

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,28 @@
-import { View, Text } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import { StatusBar, useColorScheme } from "react-native";
 const RootLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const scheme = useColorScheme();
+
+  const myTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      card: "#101010",
+      primary: "white",
+      background: "#101010",
+    },
+  };
+  return (
+    <ThemeProvider value={myTheme}>
+      <StatusBar
+        barStyle={scheme === "dark" ? "light-content" : "dark-content"}
+        backgroundColor={scheme === "dark" ? "#101010" : "#ffffff"}
+      />
+      <Slot />
+    </ThemeProvider>
+  );
 };
 
 export default RootLayout;
