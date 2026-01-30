96b4c582a7f5d3a51239ee5453ce5f15e4c56552
sdsasdfd <unasmirza316@gmail.com>
2026-01-30 10:33:57 +0500

    set up tabs functionality

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
 
-const _layout = () => {
+const TabbarLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
-  )
-}
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notification"
+        options={{
+          title: "Notification",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
 
-export default _layout
+export default TabbarLayout;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,8 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
 const RootLayout = () => {
-  return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
-  )
-}
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
 
-export default RootLayout
+export default RootLayout;
